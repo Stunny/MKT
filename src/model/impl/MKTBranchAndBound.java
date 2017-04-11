f11faaf4410e4bb44cc9b1f9b8c8f9b00f8ca1f2
Víctor <victor@MacBook-Pro-de-Victor.local>
2017-04-11 18:39:50 +0200

    B&B

--- src/model/impl/MKTBranchAndBound.java
+++ src/model/impl/MKTBranchAndBound.java
@@ -2,12 +2,46 @@ package model.impl;
 
 import model.Configuration;
 import model.Mark;
+import model.SolutionValue;
 import model.Solver;
+import model.map.Map;
+import view.MultiKeyTreasureGUI;
 
 /**
  * Created by Alex on 08/04/2017.
  */
 public class MKTBranchAndBound implements Solver {
+
+    /**
+     * Solve GUI
+     */
+    private MultiKeyTreasureGUI gui;
+
+    /**
+     * Labirynth map
+     */
+    private Map map;
+
+    /**
+     * Configuration
+     */
+
+    /**
+     * Best configuration value
+     */
+    private SolutionValue vMillor;
+
+
+
+    private Configuration x;
+
+    /**
+     * Best configuration
+     */
+    private Configuration xMillor;
+
+
+
     @Override
     public void solve(Configuration x, int k) {
         System.out.println("ayy");
